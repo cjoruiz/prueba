69c6a7631684cbdb9a7e8c6b947c923ae93ba845
cristian ortega <cjoruiz@hotmail.es>
2023-07-25 13:10:15 -0500

    cambio de style

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eec8adc6be38407d"; }
+/******/ 		__webpack_require__.h = function() { return "048a686be8e01066"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
